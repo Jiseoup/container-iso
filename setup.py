7acf542754e67357fe759968fe56641cc93f5fce
Jiseoup <jseoup@hanyang.ac.kr>
2024-12-01 19:05:35 +0900

    feat: update url

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
 	name='iso-container',
-	version='1.0.0',
+	version='1.0.1',
 	long_description=long_description,
 	long_description_content_type = 'text/markdown',
 	description='A Python package for ISO 6346 container information and validation.',
 	author='Jiseoup',
 	author_email='jseoup@gmail.com',
-	url='https://github.com/Jiseoup/container-iso',
+	url='https://github.com/Jiseoup/iso-container',
 	license='MIT',
 	python_requires='>=3.7',
 	install_requires=[],
